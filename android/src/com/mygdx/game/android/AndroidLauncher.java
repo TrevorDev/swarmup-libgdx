139bfd2647f98908811989ea22b6cbb6ae8073d3
unknown <trevorjbaron@gmail.com>
2014-09-20 21:07:07 -0400

    fix crash on older api

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -9,36 +9,39 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.Swarmup;
 
 public class AndroidLauncher extends AndroidApplication {
-	@SuppressLint("NewApi") @Override
-	protected void onCreate (Bundle savedInstanceState) {
+	@SuppressLint("NewApi") protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		//config.hideStatusBar = true;
-		try{
-		View decorView = getWindow().getDecorView();
-		int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-	decorView.setSystemUiVisibility(uiOptions);
-		}catch(Exception e){}
+		if (android.os.Build.VERSION.SDK_INT >= 11) {
+			try{
+				View decorView = getWindow().getDecorView();
+				int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+		                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+		                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+		                | View.SYSTEM_UI_FLAG_FULLSCREEN
+		                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+			decorView.setSystemUiVisibility(uiOptions);
+				}catch(Exception e){}
+		}
+		
 		initialize(new Swarmup(), config);
 	}
 	
-	@SuppressLint("NewApi") @Override
-	protected void onResume () {
+	@SuppressLint("NewApi") protected void onResume () {
 		super.onResume();
-		try{
-			View decorView = getWindow().getDecorView();
-			int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-	                | View.SYSTEM_UI_FLAG_FULLSCREEN
-	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-		decorView.setSystemUiVisibility(uiOptions);
-			}catch(Exception e){}
+		if (android.os.Build.VERSION.SDK_INT >= 11) {
+			try{
+				View decorView = getWindow().getDecorView();
+				int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+		                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+		                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+		                | View.SYSTEM_UI_FLAG_FULLSCREEN
+		                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+			decorView.setSystemUiVisibility(uiOptions);
+				}catch(Exception e){}
+		}
 	}
 }
